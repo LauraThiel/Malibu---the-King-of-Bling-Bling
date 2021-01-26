d675bddec209ef6b507c04ade4b8c6c27e1cbed3
Loreley <Laura.Thiel.Me@gmail.com>
2021-01-26 11:59:35 +0100

    added functions to repeat elements

--- game.js
+++ game.js
@@ -15,55 +15,129 @@ let ballImg = document.createElement('img')
 ballImg.src = '/images/ball.JPG'
 let dogImg = document.createElement('img')
 dogImg.src = '/images/ball.JPG'
-let obstacles =[{x: canvas.width -60, y: 0}]
+let grass =[{x: 0, y: 450}, {x: 500, y: 450}]
+let fences =[{x: -200, y: 400}]
+let treats =[{x: -200, y: 200}]
+let balls =[{x: -200, y: 150}]
 let fenceX = 800
+let ballX = 1200
+let treatX = 1100
 let fgX = 0
 let fg2X = 500
+let fg3X = 1000
 let dogY = 440
-let dogIncrement = 2
-let dogArea = 30
+//let dogIncrement = 440
+let dogArea = 50
+
+
+let startBtn = document.querySelector('#start')
 
 document.addEventListener("mousedown", () => {
-    dogIncrement =-30
+    dogY = dogY - 100
 })
 
 document.addEventListener("mouseup", () => {
-    dogIncrement = 10
+    dogY = dogY + 100
 })
 
 function draw(){
     ctx.drawImage(backImg, 0, 0)
-    //for(let i=0)
-    ctx.drawImage(fenceImg, fenceX, 415)
-    ctx.drawImage(fgImg, fgX, 450)
-    ctx.drawImage(fgImg, fg2X, 450)
-    ctx.drawImage(ballImg, 500, 200)
+    //ctx.drawImage(fenceImg, fenceX, 415)
+ 
     ctx.drawImage(dogImg, 150, dogY)
-    ctx.drawImage(treatImg, 500, 200)
     ctx.font = '20px Verdana'
     ctx.fillText('Score: ' + score, 10, 30)
     fenceX -= 20
+    ballX -= 20
+    treatX -= 20
     fgX -= 20
     fg2X -= 20
-    dogY += dogIncrement
+    //dogY = dogIncrement
+    runningFences()
+    runningGrass()
+    runningTreats()
+    runningBalls()
+    //dogOnGrass()
 
 }
 
-
-function dogOnGrass (){
-    if (dogY + dogArea > canvas.height) {
-        dogIncrement = 0
+function runningGrass () {
+    for(let i=0; i< grass.length; i++){
+        ctx.drawImage(fgImg, grass[i].x, grass[i].y)
+        grass[i].x -= 20
     }
+    if (grass[grass.length-1].x <600) {
+        grass.push({
+            x: canvas.width,
+            y: 450
+        })
+    }    
+ //    if (grass[grass[0]].x <-500) {
+ //       grass.remove(grass[0])         //NEEDS TO BE CHECKED
+ //   }
 }
 
+function runningFences () {
+    for(let i=0; i< fences.length; i++){
+        ctx.drawImage(fenceImg, fences[i].x, fences[i].y)
+        fences[i].x -= 20
+    }
+    if (fences[fences.length-1].x <600) {
+        fences.push({
+            x: canvas.width,
+            y: 420
+        })
+    }    
+
+}
+
+function runningTreats () {
+    for(let i=0; i< treats.length; i++){
+        ctx.drawImage(treatImg, treats[i].x, treats[i].y)
+        treats[i].x -= 20
+    }
+    if (treats[treats.length-1].x <800) {
+        treats.push({
+            x: canvas.width,
+            y: 220
+        })
+    }    
+
+}
+
+function runningBalls () {
+    for(let i=0; i< balls.length; i++){
+        ctx.drawImage(ballImg, balls[i].x, balls[i].y)
+        balls[i].x -= 20
+    }
+    if (balls[balls.length-1].x <600) {
+        balls.push({
+            x: canvas.width,
+            y: 220
+        })
+    }    
+
+}
+
+//function dogOnGrass (){
+//    if (dogY + dogArea <440) {
+//        dogIncrement = 2
+//    }
+//    else dogIncrement = 0
+//}
 
 
+function gameOver(){
+    canvas.style.display = 'none'
+}
 
-
-
-
-
-
+function startGame(){
+    canvas.style.display = 'block'
+    startBtn.style.display = 'none'
+    intervalID = setInterval(() => {
+        requestAnimationFrame(draw)
+    }, 100)
+}
 
 
 // create interval here
@@ -73,5 +147,5 @@ function dogOnGrass (){
 window.addEventListener('load', () => {
     intervalID = setInterval(() => {
         requestAnimationFrame(draw)
-     }, 100)
+     }, 400)
 })
