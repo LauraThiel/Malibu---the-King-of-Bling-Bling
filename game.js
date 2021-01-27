91268c93e591f742a252597e9b9bbd6001029626
Loreley <Laura.Thiel.Me@gmail.com>
2021-01-27 09:09:32 +0100

    almost mvp

--- game.js
+++ game.js
@@ -12,39 +12,48 @@ treatImg.src = '/images/treat.JPG'
 let fenceImg = document.createElement('img')
 fenceImg.src = '/images/fence.png'
 let ballImg = document.createElement('img')
-ballImg.src = '/images/ball.JPG'
+ballImg.src = '/images/ball.png'
 let dogImg = document.createElement('img')
 dogImg.src = '/images/ball.JPG'
+let houseImg = document.createElement('img')
+houseImg.src = '/images/house.png'
 let grass =[{x: 0, y: 450}, {x: 500, y: 450}]
 let fences =[{x: -200, y: 400}]
-let treats =[{x: -200, y: 200}]
+let treats =[{x: 400, y: 200}]
 let balls =[{x: -200, y: 150}]
 let fenceX = 800
 let ballX = 1200
 let treatX = 1100
 let fgX = 0
 let fg2X = 500
-let fg3X = 1000
 let dogY = 440
-//let dogIncrement = 440
+let dogX = 150
+let dogIncrement = 5  
 let dogArea = 50
+let houseX = 2000
+let houseY = 275
 
 
 let startBtn = document.querySelector('#start')
 
-document.addEventListener("mousedown", () => {
-    dogY = dogY - 100
+
+document.addEventListener("keydown", event => {
+    if (event.keyCode == 32 || event.key == " ") {
+        dogIncrement -= 5
+    }
+} )
+
+document.addEventListener("keyup", event => {
+    if (event.keyCode == 32 || event.key == " ") {
+        dogIncrement = 5
+    }
 })
 
-document.addEventListener("mouseup", () => {
-    dogY = dogY + 100
-})
 
 function draw(){
-    ctx.drawImage(backImg, 0, 0)
-    //ctx.drawImage(fenceImg, fenceX, 415)
- 
-    ctx.drawImage(dogImg, 150, dogY)
+    ctx.drawImage(backImg, 0, 0) 
+    ctx.drawImage(dogImg, dogX, dogY)
+    ctx.drawImage(houseImg, houseX, houseY)
     ctx.font = '20px Verdana'
     ctx.fillText('Score: ' + score, 10, 30)
     fenceX -= 20
@@ -52,12 +61,14 @@ function draw(){
     treatX -= 20
     fgX -= 20
     fg2X -= 20
-    //dogY = dogIncrement
+    dogY += dogIncrement
+    houseX -= 20
     runningFences()
     runningGrass()
     runningTreats()
     runningBalls()
-    //dogOnGrass()
+    dogOnGrass()
+    gettingHome()
 
 }
 
@@ -73,33 +84,51 @@ function runningGrass () {
         })
     }    
  //    if (grass[grass[0]].x <-500) {
- //       grass.remove(grass[0])         //NEEDS TO BE CHECKED
+ //       grass.remove(grass[0])         //NEEDS TO BE CHECKED AND ADDED TO THE OTHER FUNCTIONS
  //   }
 }
+
+
+
+
 
 function runningFences () {
     for(let i=0; i< fences.length; i++){
         ctx.drawImage(fenceImg, fences[i].x, fences[i].y)
         fences[i].x -= 20
+        //Collision dog and fence
+        if((dogX + dogImg.width < fences[i].x + fenceImg.width && dogX > fences[i].x ) &&
+         (dogY + dogImg.height > fences[i].y  && dogY < fences[i].y + fenceImg.height)){
+            clearInterval(intervalID);
+            gameOver()
+        }
     }
     if (fences[fences.length-1].x <600) {
         fences.push({
-            x: canvas.width,
+            x: Math.floor(Math.random() * canvas.width + 500),
             y: 420
         })
     }    
 
 }
 
+
 function runningTreats () {
     for(let i=0; i< treats.length; i++){
         ctx.drawImage(treatImg, treats[i].x, treats[i].y)
         treats[i].x -= 20
+        //Collision dog and treats
+        if((dogX + dogImg.width < treats[i].x + treatImg.width && dogX > treats[i].x ) &&
+         (dogY + dogImg.height > treats[i].y  && dogY < treats[i].y + treatImg.height)){
+            treats.splice(i, 1);
+            i--
+            score ++
+        }     
     }
-    if (treats[treats.length-1].x <800) {
+    if (treats[treats.length-1].x <900) {
         treats.push({
-            x: canvas.width,
-            y: 220
+            x: canvas.width + 30,
+            y: Math.floor(Math.random() * canvas.height - 300)
         })
     }    
 
@@ -109,25 +138,42 @@ function runningBalls () {
     for(let i=0; i< balls.length; i++){
         ctx.drawImage(ballImg, balls[i].x, balls[i].y)
         balls[i].x -= 20
+        //Collision dog and ball
+        if((dogX + dogImg.width < balls[i].x + ballImg.width && dogX > balls[i].x ) && 
+        (dogY + dogImg.height > balls[i].y  && dogY < balls[i].y + ballImg.height)){
+            clearInterval(intervalID);
+            gameOver()
+        }
     }
-    if (balls[balls.length-1].x <600) {
+    if (balls[balls.length-1].x <500) {
         balls.push({
-            x: canvas.width,
-            y: 220
+            x: canvas.width + 30,
+            y: Math.floor(Math.random() * canvas.height - 200)
         })
     }    
 
 }
 
-//function dogOnGrass (){
-//    if (dogY + dogArea <440) {
-//        dogIncrement = 2
-//    }
-//    else dogIncrement = 0
-//}
+function dogOnGrass (){
+    if (dogY + dogArea <canvas.height - 30) {
+        dogIncrement = 5
+    }
+    else dogIncrement = 0
+}
+
+function gettingHome(){
+    if(dogX + dogImg.width > houseX) {
+        clearInterval(intervalID);
+        gameWon()
+    }
+}
 
 
 function gameOver(){
+    canvas.style.display = 'none'
+}
+
+function gameWon(){
     canvas.style.display = 'none'
 }
 
@@ -143,9 +189,21 @@ function startGame(){
 // create interval here
 
 
-
-window.addEventListener('load', () => {
+function startGame(){
+    canvas.style.display = 'block'
+    startBtn.style.display = 'none'
     intervalID = setInterval(() => {
         requestAnimationFrame(draw)
-     }, 400)
+    }, 400)
+}
+
+
+//window.addEventListener('load', () => {
+//    intervalID = setInterval(() => {
+//        requestAnimationFrame(draw)
+//     }, 400)
+//})
+
+startBtn.addEventListener('click', () => {
+    startGame()
 })
